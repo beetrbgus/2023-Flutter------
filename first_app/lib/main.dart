0218c2ea0c03c814f8d0f9debb23a5a1e8892949
beetrbgus <devbeetrb0214@gmail.com>
2023-10-15 02:30:19 +0900

    ### 요약 - Widget은 객체이고, Widget 안에는 대개 인수들이 전달된다. required인 것도 있고, optional인 것도 있다. - background의 Color는 dart의 Color 객체이다.

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
+      backgroundColor: Color.fromARGB(255, 153, 186, 242),
       body: Center(child: Text('Hello World')),
     ),
   ));
