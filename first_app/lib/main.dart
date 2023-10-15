fd42b2d1067a1788e522b8819da142a08598be08
beetrbgus <devbeetrb0214@gmail.com>
2023-10-15 23:48:24 +0900

    ###Container  class로 분리

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,25 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.orangeAccent, Color.fromARGB(255, 235, 227, 217)],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        child: const Center(
-          child: Text(
-              style: TextStyle(
-                color: Colors.white70,
-                fontSize: 28,
-              ),
-              'Hello World'),
-        ),
-      ),
+      body: GradientContainer(),
     ),
   ));
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.orangeAccent,
+            Color.fromARGB(255, 235, 227, 217),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+            style: TextStyle(
+              color: Colors.white70,
+              fontSize: 28,
+            ),
+            'Hello World'),
+      ),
+    );
+  }
 }
