1c76bb27f1f05db7f3508b0bcdea4e631e7ec931
beetrbgus <devbeetrb0214@gmail.com>
2023-10-15 22:55:26 +0900

    ### 텍스트 위젯 스타일 추가 - 텍스트 위젯의 스타일을 추가해 봄

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -4,14 +4,22 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Color.fromARGB(255, 235, 227, 217), Colors.orangeAccent],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.orangeAccent, Color.fromARGB(255, 235, 227, 217)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
           ),
-          child: const Center(child: Text('Hello World'))),
+        ),
+        child: const Center(
+          child: Text(
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 28,
+              ),
+              'Hello World'),
+        ),
+      ),
     ),
   ));
 }
