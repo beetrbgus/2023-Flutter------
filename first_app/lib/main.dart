2c64bbd951b102311672c1eca7b875c3b29f0c9c
beetrbgus <devbeetrb0214@gmail.com>
2023-10-15 22:27:47 +0900

    ### 그라데이션 추가 - begin으로 시작점 - end로 끝 점 추가 - 그라데이션은 colors의 0번째부터 begin -> end의 순서대로 감

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -5,10 +5,12 @@ void main() {
     home: Scaffold(
       body: Container(
           decoration: const BoxDecoration(
-              gradient: LinearGradient(colors: [
-            Color.fromARGB(255, 235, 227, 217),
-            Colors.orangeAccent
-          ])),
+            gradient: LinearGradient(
+              colors: [Color.fromARGB(255, 235, 227, 217), Colors.orangeAccent],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
           child: const Center(child: Text('Hello World'))),
     ),
   ));
