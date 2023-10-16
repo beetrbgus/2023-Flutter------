77d58771d7ca2574c3744c06ab67342c0f92a4de
beetrbgus <devbeetrb0214@gmail.com>
2023-10-16 20:17:02 +0900

    final vs cosnt - const : 컴파일 시점에 이미 값이 정해짐 - final : 사용 시점에 값이 정해짐
    해당 부분은 컴파일 시점에 값이 정해져 불변하기 때문에 const로 작성

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,6 +1,9 @@
 import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
+const Alignment topLeft = Alignment.topLeft;
+const Alignment bottomRight = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
@@ -13,8 +16,8 @@ class GradientContainer extends StatelessWidget {
             Colors.orangeAccent,
             Color.fromARGB(255, 235, 227, 217),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: topLeft,
+          end: bottomRight,
         ),
       ),
       child: const Center(
