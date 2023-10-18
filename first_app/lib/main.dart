18c8ed31ff8a6bb149416be3d61160764d2b1612
beetrbgus <devbeetrb0214@gmail.com>
2023-10-18 21:37:50 +0900

    named 생성자 적용

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -4,10 +4,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
-      body: GradientContainer(
-        Color.fromRGBO(255, 171, 64, 1),
-        Color.fromARGB(255, 235, 227, 217),
-      ),
+      body: GradientContainer.orange(),
     ),
   ));
 }
