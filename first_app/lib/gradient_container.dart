b4e8676be7a8c1c926a70123c6761b6d0ce682fb
beetrbgus <devbeetrb0214@gmail.com>
2023-10-25 00:04:02 +0900

    정렬하기 - Column의 mainAxisSize를 통해서 위에 있는 다이스 아래로 내려오기
    - 위젯과 위젯 사이를 SizedBox로 여백을 주기
    
    - 텍스트 버튼 스타일 만들기

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -32,12 +32,24 @@ class GradientContainer extends StatelessWidget {
       ),
       child: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(
               'assets/images/dice-2.png',
               width: 200,
             ),
-            TextButton(onPressed: rollDice, child: const Text('주사위 굴리기'))
+            const SizedBox(
+              height: 20,
+            ),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.only(top: 30),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(fontSize: 28),
+              ),
+              child: const Text('주사위 굴리기'),
+            )
           ],
         ),
       ),
