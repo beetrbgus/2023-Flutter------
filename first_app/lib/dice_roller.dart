5f70a99b632659d74fb187c78d441fd35a0cdaff
beetrbgus <devbeetrb0214@gmail.com>
2023-10-25 21:59:58 +0900

    랜덤 숫자로 주사위 굴리는 것 추가

--- first_app/lib/dice_roller.dart
+++ first_app/lib/dice_roller.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 // stateful 위젯은 두개의 클래스로 구성
@@ -14,13 +16,13 @@ class DiceRoller extends StatefulWidget {
 
 // _ 로 private 접근자라는 것을 알려줌.
 class _DiceRollerState extends State<DiceRoller> {
-  var activeDiceImage = 'assets/images/dice-1.png';
+  var currentDiceRoll = 2;
 
   // 주사위 굴리면 나오는 행동을 정의
   void rollDice() {
     // stateful 위젯에서 build를 다시 실행하라고 명령함.
     setState(() {
-      activeDiceImage = 'assets/images/dice-4.png';
+      currentDiceRoll = (Random().nextInt(6) + 1);
     });
   }
 
@@ -30,7 +32,7 @@ class _DiceRollerState extends State<DiceRoller> {
       mainAxisSize: MainAxisSize.min,
       children: [
         Image.asset(
-          activeDiceImage,
+          'assets/images/dice-$currentDiceRoll.png',
           width: 200,
         ),
         const SizedBox(
