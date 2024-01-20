e79fe2b83ce3ec2afa27a3147c10abd7b4aec5c4
beetrbgus <devbeetrb0214@gmail.com>
2024-01-21 00:01:30 +0900

    feat - 카테고리 추가
    - enum 클래스를 이용한 비용 사용처 추가

--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -13,10 +13,20 @@ class Expense {
     required this.title,
     required this.amount,
     required this.createdAt,
+    required this.category,
   }) : id = uuId.v8();
 
   final String id;
   final String title; // 제목
   final double amount; // 비용
   final DateTime createdAt; //  생성일
+  final Category category; // 사용처
+}
+
+enum Category {
+  food,
+  travel,
+  leisure,
+  work,
+  ;
 }
