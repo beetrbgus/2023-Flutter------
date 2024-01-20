db74d12f77203dee713a83cb801fa505d3c122d8
beetrbgus <devbeetrb0214@gmail.com>
2024-01-21 07:45:57 +0900

    feat - 목록 카드형식 추가
    - expense 패키지 이동
    - format 의존성 추가
    - 목록을 반환하는 위젯 생성

---
+++ expense_tracker/lib/widgets/expense.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
+
+// ignore: slash_for_doc_comments
+/**
+ * 비용
+ */
+const uuId = Uuid();
+final dateformatter = DateFormat("yyyy-MM-dd");
+// 강의에서 나온 enum을 Icon으로 처리한 것
+const categoryIconse = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff_rounded,
+  Category.leisure: Icons.sports_football_outlined,
+  Category.work: Icons.card_travel,
+};
+
+class Expense {
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.createdAt,
+    required this.category,
+  }) : id = uuId.v8();
+
+  final String id;
+  final String title; // 제목
+  final double amount; // 비용
+  final DateTime createdAt; //  생성일
+  final Category category; // 사용처
+
+  String get formmattedCreatedAt {
+    return dateformatter.format(createdAt);
+  }
+}
+
+enum Category {
+  food(Icons.lunch_dining),
+  travel(Icons.flight_takeoff_rounded),
+  leisure(Icons.sports_football_outlined),
+  work(Icons.card_travel),
+  ;
+
+  const Category(this.icon);
+  final IconData icon;
+}
---
+++ expense_tracker/lib/widgets/expenses_list/expenses_item.dart
@@ -0,0 +1,55 @@
+import 'package:expense_tracker/widgets/expense.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(
+    this.expense, {
+    super.key,
+  });
+
+  final Expense expense;
+  @override
+  Widget build(BuildContext context) {
+    var format = NumberFormat("###,###,###,###");
+    return Card(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 16,
+            ),
+            child: Column(
+              children: [
+                Text(
+                  expense.title,
+                ),
+                const SizedBox(height: 4),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text('￦ ${format.format(expense.amount)}'),
+
+                    const Spacer(), // 남은 공간을 다 차지, 위의 amount는 왼쪽, Row는 오른쪽으로 밀어냄
+                    Row(children: [
+                      Icon(
+                        categoryIconse[expense.category],
+                      ), // 강의에서 나오는 enum을 맵으로 만드는 방식
+                      // Icon(expense.category.icon), // 이 방법으로도 사용 가능
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Text(expense.formmattedCreatedAt),
+                    ]),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
