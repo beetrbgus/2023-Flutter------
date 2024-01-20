d79722d21026d68bbb198576223a1ca977aeeebb
beetrbgus <devbeetrb0214@gmail.com>
2024-01-21 01:01:12 +0900

    feat - 비용 목록 리스트 출력

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -1,3 +1,5 @@
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -8,13 +10,44 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registerExpenses = [
+    Expense(
+      title: "더미 제목1",
+      amount: 10000,
+      createdAt: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: "더미 제목2",
+      amount: 7000,
+      createdAt: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: "더미 제목3",
+      amount: 8000,
+      createdAt: DateTime.now(),
+      category: Category.travel,
+    ),
+    Expense(
+      title: "더미 제목4",
+      amount: 3000,
+      createdAt: DateTime.now(),
+      category: Category.work,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Text('차트'),
-          Text('비용 목록'),
+          const Text('차트'),
+          Expanded(
+            child: ExpensesList(
+              expenses: _registerExpenses,
+            ),
+          ),
         ],
       ),
     );
