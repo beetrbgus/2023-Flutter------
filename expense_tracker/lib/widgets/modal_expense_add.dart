319ff8f8cee003976eba9a92fdfe4e0b98038e80
beetrbgus <devbeetrb0214@gmail.com>
2024-01-25 22:06:59 +0900

    feat - datepicker 추가

--- expense_tracker/lib/widgets/modal_expense_add.dart
+++ expense_tracker/lib/widgets/modal_expense_add.dart
@@ -13,6 +13,18 @@ class _ModalExpenseAdd extends State<ModalExpenseAdd> {
   // 텍스트 필드에 값으로 전달되는 값을 저장하는 것을 알아서 해 줌.
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  void _presentDatePicker() {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+
+    showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+  }
+
   //initState와 build와 비슷하다. StatefulWidget의 생명주기의 일부분이다.
   // Widget 과 state가 destroy 되면 자동으로 플러터에 의해 호출된다.
   @override
@@ -40,19 +52,43 @@ class _ModalExpenseAdd extends State<ModalExpenseAdd> {
               label: Text("제목"),
             ),
           ),
-          TextField(
-            controller: _amountController,
-            maxLength: 50,
-            keyboardType: TextInputType.number,
-            decoration: const InputDecoration(
-              prefixText: '￦',
-              label: Text("비용"),
-            ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  maxLength: 50,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    prefixText: '￦',
+                    label: Text("비용"),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text("사용일"),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(
+                        Icons.date_range_outlined,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
           Row(
             children: [
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context);
+                },
                 child: const Icon(
                   Icons.cancel_presentation_outlined,
                 ),
