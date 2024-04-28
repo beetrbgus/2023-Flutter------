f98df1e3e0febed1d6f444a10339880a734f582a
beetrbgus <devbeetrb0214@gmail.com>
2024-04-28 23:30:57 +0900

    feat - 그리드 뷰 항목 추가.
    - 모양 변경
    - 더미데이터로 추가

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'package:meals/category.dart';
+import 'package:meals/screens/category.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
