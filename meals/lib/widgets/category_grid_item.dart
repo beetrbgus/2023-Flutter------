04576816fcfad05f6e5e63cd86d14454a02b583c
beetrbgus <devbeetrb0214@gmail.com>
2024-04-29 00:01:31 +0900

    feat - 클릭시 print 추가

--- meals/lib/widgets/category_grid_item.dart
+++ meals/lib/widgets/category_grid_item.dart
@@ -10,25 +10,30 @@ class CategoryGridItem extends StatelessWidget {
   final Category category;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(
-        16,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.55),
-            category.color.withOpacity(0.9),
-          ],
-          begin: Alignment.bottomRight,
+    return InkWell(
+      onTap: () {
+        print("zz");
+      },
+      child: Container(
+        padding: const EdgeInsets.all(
+          16,
         ),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-              color: Theme.of(context).colorScheme.background,
-            ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ],
+            begin: Alignment.bottomRight,
+          ),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.background,
+              ),
+        ),
       ),
     );
   }
