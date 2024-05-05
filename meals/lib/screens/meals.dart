b895837197be5414608517157ceb23a94f1961ee
beetrbgus <devbeetrb0214@gmail.com>
2024-05-06 02:35:54 +0900

    feat - 음식 상세 화면 추가

--- meals/lib/screens/meals.dart
+++ meals/lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meal_details.dart';
 import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
@@ -7,6 +8,16 @@ class MealsScreen extends StatelessWidget {
 
   final String title;
   final List<Meal> meals;
+
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailScreen(
+          meal: meal,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,8 +46,11 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (context, index) => MealItem(
+        itemBuilder: (ctx, index) => MealItem(
           meal: meals[index],
+          onSelectMeal: (context, meal) {
+            selectMeal(ctx, meal);
+          },
         ),
       );
     }
