3dc8c02a149ce95528ece54cc6e1c8daee4af057
beetrbgus <devbeetrb0214@gmail.com>
2024-05-19 23:59:44 +0900

    feat - 필터 프로바이더

---
+++ meals/lib/providers/filters_provider.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/screens/filters.dart';
+
+enum Filter {
+  glutenFree,
+  lactoseFree,
+  vegetarian,
+  vegan,
+}
+
+class FilterNotifier extends StateNotifier<Map<Filter, bool>> {
+  FilterNotifier()
+      : super({
+          Filter.glutenFree: false,
+          Filter.lactoseFree: false,
+          Filter.vegetarian: false,
+          Filter.vegan: false,
+        });
+
+  void setFiulter(Filter filter, bool isActive) {
+    // state[filter] = isActive; //허용 안됨 => state 메모리 변경 X
+    state = {
+      ...state,
+      filter: isActive,
+    };
+  }
+}
+
+final filterProvider = StateNotifierProvider<FilterNotifier, Map<Filter, bool>>(
+  (ref) => FilterNotifier(),
+);
